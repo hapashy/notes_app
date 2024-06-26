de5c1075fdbf3198cc406f34e029ec6ca8e6b31c
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-26 16:55:43 +0300

    add hive

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -17,7 +17,7 @@ class NoteItem extends StatelessWidget {
         );
       },
       child: Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 8, left: 16),
+        padding: const EdgeInsets.only(top: 8, bottom: 2, left: 16),
         decoration: BoxDecoration(
           color: const Color(0xffFFCCB0),
           borderRadius: BorderRadius.circular(16),
